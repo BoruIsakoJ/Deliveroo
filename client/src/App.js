47e9c3f433b59999203ece97c008485c9215f975
Boru Isako <isco.jaldy@gmail.com>
2025-07-21 20:45:27 +0300

    Added Page Routing that will direct users to the respective pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/landingPage/LandingPage';
 import Navbar from './components/navbar/Navbar';
+import UserNavBar from './components/userNavbar/UserNavbar';
+import Login from './pages/login/Login';
 
 function App() {
+  const login = false
   return (
     <Router>
       <div className="App">
-        <Navbar/>
+        {login?<UserNavBar />:<Navbar/>}
         <div className='content'>
           <Routes>
             <Route path='/' element={<LandingPage />} />
+            <Route path='/login' element={<Login/>}/>
           </Routes>
         </div>
       </div>

1e0d53bf3fc5f698bbc75e258a7f31ec8b2b0643
Joel Mwaga <mwagahjoel14@gmail.com>
2025-07-28 18:38:58 +0300

    fixed the landing page

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,27 +2,38 @@ import React from 'react'
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import HomeFilledIcon from '@mui/icons-material/HomeFilled';
 import './navbar.css';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className='navbar'>
-        <Link to='/' className='navbar-left'>
-          <LocalShippingIcon className='company-logo' />
-          <p>Deliveroo</p>
+    <nav className='custom-navbar'>
+      <div className='custom-navbar-left'>
+        <Link to='/' className='custom-brand'>
+          <LocalShippingIcon className='custom-logo' />
+          <span>Deliveroo</span>
         </Link>
+      </div>
 
-      <Link to="/" className='navbar-center'>
-        <HomeFilledIcon className='home-logo' />
-        <p>Home</p>
-      </Link>
-      <div className='navbar-right'>
-        <Link to='/track'><button className='track-order-btn'>Track Order</button></Link>
-        <Link to='/login'><button className='sign-in-btn'>Log In</button></Link>
-        <Link to='/signup'><button className='get-started-btn'>Get Started</button></Link>
+      <div className='custom-navbar-center'>
+        <Link to="/" className='custom-nav-link'>
+          <HomeFilledIcon className='custom-icon' />
+          <span>Home</span>
+        </Link>
+      </div>
+
+      <div className='custom-navbar-right'>
+        <Link to='/track'>
+          <button className='custom-btn outline-btn'>Track Order</button>
+        </Link>
+        <Link to='/login'>
+          <button className='custom-btn ghost-btn'>Log In</button>
+        </Link>
+        <Link to='/signup'>
+          <button className='custom-btn filled-btn'>Get Started</button>
+        </Link>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
